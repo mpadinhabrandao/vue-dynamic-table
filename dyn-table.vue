0e668389bae159a5afc5177e4ed52668c44b98e1
root <root@vps15.bsolus.pt>
2017-10-05 15:19:08 +0000

    move files to folder src

--- dyn-table.vue
+++ dyn-table.vue
@@ -1,3 +1,3 @@
-<template   src="./dyn-table.html"  lang="html" />
-<style      src="./dyn-table.css"   lang="css"  scope />
-<script     src="./dyn-table.js"    lang="js"   />
+<template   src="./src/dyn-table.html"  lang="html" />
+<style      src="./src/dyn-table.css"   lang="css"  scope />
+<script     src="./src/dyn-table.js"    lang="js"   />
