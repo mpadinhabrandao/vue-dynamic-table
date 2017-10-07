513357d64e8b10edae37d1ee537f102301c531d2
root <root@vps15.bsolus.pt>
2017-10-07 16:01:14 +0000

    #1

--- dyn-table.vue
+++ dyn-table.vue
@@ -1,3 +1,3 @@
 <template   src="./src/dyn-table.html"  lang="html" />
-<style      src="./src/dyn-table.css"   lang="css"  scope />
+<style      src="./src/dyn-table.css"   lang="css"  scoped />
 <script     src="./src/dyn-table.js"    lang="js"   />
